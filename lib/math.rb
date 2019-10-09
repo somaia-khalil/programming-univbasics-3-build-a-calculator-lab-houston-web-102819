74493e7134891178ff7a4f76781d00588cf21a2e
somaia khalil <sumaya_khair@live.com>
2019-10-09 04:07:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,18 +1,17 @@
 def addition(num1, num2)
- sum = num2 + num1
- puts sum
+  num2 + num1
 end
 
 def subtraction(num1, num2)
-
+  num2 - num1
 end
 
 def division(num1, num2)
-
+ num2 / num1
 end
 
 def multiplication(num1, num2)
-
+num2 * num1
 end
 
 def modulo(num1, num2)
